3a8f9394351532b5b204a8711f84ee147931d0ba
unknown <prasetysetiko@gmail.com>
2020-01-27 16:26:20 +0700

    myReact

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import TinodeWeb from 'tinode-webapp';
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <TinodeWeb />
+      </div>
+
+    );
+  }
+}
+
+export default App;
